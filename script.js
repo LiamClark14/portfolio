f1ed53f290446144dac55fe12b02ca82d9e12a5f
LIAM CLARK <liamclark@duke.edu>
2021-08-07 16:30:29 -0400

    added responsiveness to main btn

--- script.js
+++ script.js
@@ -16,30 +16,30 @@ const mainImg = document.querySelector(".main-circle img")
 let mX = 0;
 let mY = 0;
 const z = 75;
-const animateCircles = (e,x,y) => {
-  if(x < mX){
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
     circles.forEach(circle => {
       circle.style.left = `${z}px`;
     });
     mainImg.style.left = `${z}px`;
-  } else if(x > mX){
+  } else if (x > mX) {
     circles.forEach(circle => {
       circle.style.left = `-${z}px`;
     });
     mainImg.style.left = `-${z}px`;
   }
-  if(y < mY){
+  if (y < mY) {
     circles.forEach(circle => {
       circle.style.top = `${z}px`;
     });
     mainImg.style.top = `${z}px`;
-  }else if(y > mY){
+  } else if (y > mY) {
     circles.forEach(circle => {
       circle.style.top = `-${z}px`;
     });
     mainImg.style.top = `-${z}px`;
   }
-  
+
   mX = e.clientX;
   mY = e.clientY;
 }
@@ -50,10 +50,31 @@ document.body.addEventListener("mousemove", (e) => {
   let y = e.clientY;
 
   mouseCircleFn(x, y);
-  animateCircles(e,x,y);
+  animateCircles(e, x, y);
 });
 
 document.body.addEventListener('mouseleave', () => {
   mouseCircle.style.opacity = "0";
   mouseDot.style.opacity = "0";
 })
+
+// Main button
+const mainBtn = document.querySelector('.main-btn');
+
+let ripple;
+
+mainBtn.addEventListener('mouseenter', (e) => {
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+
+  ripple = document.createElement('div');
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  mainBtn.prepend(ripple);
+})
+
+mainBtn.addEventListener('mouseleave', () => {
+  mainBtn.removeChild(ripple);
+})
+// End of Main button
